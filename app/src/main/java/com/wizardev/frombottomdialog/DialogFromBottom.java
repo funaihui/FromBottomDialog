9d5430e54977888d1a8993900d4e485e81e2d4c7
wizardev <bunenggou119@gmail.com>
2017-09-15 15:08:20 +0800

    三种弹出方式编写完成

---
+++ app/src/main/java/com/wizardev/frombottomdialog/DialogFromBottom.java
@@ -0,0 +1,169 @@
+package com.wizardev.frombottomdialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.TranslateAnimation;
+
+import com.wizardev.frombottomdialog.utils.DisplayHelper;
+
+/**
+ * author : wizardev
+ * e-mail : wizarddev@163.com
+ * time   : 2017/09/15
+ * desc   :
+ * version: 1.0
+ */
+public class DialogFromBottom extends Dialog {
+    private final static int mAnimationDuration = 200;
+    // 持有 ContentView，为了做动画
+    private View mContentView;
+    private boolean mIsAnimating = false;
+
+    private OnBottomSheetShowListener mOnBottomSheetShowListener;
+
+    public DialogFromBottom(@NonNull Context context) {
+        super(context, R.style.AppTheme_BottomSheet);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setPadding(0, 0, 0, 0);
+
+        // 在底部，宽度撑满
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        params.gravity = Gravity.BOTTOM | Gravity.CENTER;//dialog从哪里弹出
+        //弹出窗口的宽高
+        int screenWidth = DisplayHelper.getScreenWidth(getContext());
+        int screenHeight = DisplayHelper.getScreenHeight(getContext());
+        params.width = screenWidth < screenHeight ? screenWidth : screenHeight;
+        getWindow().setAttributes(params);
+        setCanceledOnTouchOutside(true);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        mContentView = LayoutInflater.from(getContext()).inflate(layoutResID, null);
+        super.setContentView(mContentView);
+    }
+
+    @Override
+    public void setContentView(@NonNull View view) {
+        mContentView = view;
+        super.setContentView(view);
+    }
+
+    @Override
+    public void setContentView(@NonNull View view, ViewGroup.LayoutParams params) {
+        mContentView = view;
+        super.setContentView(view, params);
+    }
+
+    /**
+     * BottomSheet升起动画
+     */
+    private void animateUp() {
+        if (mContentView == null) {
+            return;
+        }
+        TranslateAnimation translate = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, 0f,
+                Animation.RELATIVE_TO_SELF, 1f, Animation.RELATIVE_TO_SELF, 0f
+        );
+        AlphaAnimation alpha = new AlphaAnimation(0, 1);
+        AnimationSet set = new AnimationSet(true);
+        set.addAnimation(translate);
+        set.addAnimation(alpha);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.setDuration(mAnimationDuration);
+        set.setFillAfter(true);
+        mContentView.startAnimation(set);
+    }
+
+    /**
+     * BottomSheet降下动画
+     */
+    private void animateDown() {
+        if (mContentView == null) {
+            return;
+        }
+        TranslateAnimation translate = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, 0f,
+                Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, 1f
+        );
+        AlphaAnimation alpha = new AlphaAnimation(1, 0);
+        AnimationSet set = new AnimationSet(true);
+        set.addAnimation(translate);
+        set.addAnimation(alpha);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.setDuration(mAnimationDuration);
+        set.setFillAfter(true);
+        set.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                mIsAnimating = true;
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mIsAnimating = false;
+                /**
+                 * Bugfix： Attempting to destroy the window while drawing!
+                 */
+                mContentView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // java.lang.IllegalArgumentException: View=com.android.internal.policy.PhoneWindow$DecorView{22dbf5b V.E...... R......D 0,0-1080,1083} not attached to window manager
+                        // 在dismiss的时候可能已经detach了，简单try-catch一下
+                        try {
+                            DialogFromBottom.super.dismiss();
+                        } catch (Exception e) {
+                            //     QMUILog.w(TAG, "dismiss error\n" + Log.getStackTraceString(e));
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mContentView.startAnimation(set);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        animateUp();
+        if (mOnBottomSheetShowListener != null) {
+            mOnBottomSheetShowListener.onShow();
+        }
+    }
+
+    @Override
+    public void dismiss() {
+        if (mIsAnimating) {
+            return;
+        }
+        animateDown();
+    }
+
+    public interface OnBottomSheetShowListener {
+        void onShow();
+    }
+
+}
--- app/src/main/java/com/wizardev/frombottomdialog/MainActivity.java
+++ app/src/main/java/com/wizardev/frombottomdialog/MainActivity.java
@@ -1,5 +1,7 @@
-package com.wizardev.popdialogfrombottom;
+package com.wizardev.frombottomdialog;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import java.lang.reflect.Field;
 
@@ -17,6 +20,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private View contentView;
     private PopupWindow popupWindow;
     private Button mPopupAction;
+    private Button mDialog;
+    private Button mFragment;
+    private DialogFromBottom dialogFromBottom;
+    private View dialogContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +33,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initView() {
+        dialogContent = LayoutInflater.from(this).inflate(R.layout.dialog_layout, null, false);
         initPopupWindow();
         mPopupAction = findViewById(R.id.popup_window);
         mPopupAction.setOnClickListener(this);
+        mDialog = findViewById(R.id.dialog);
+        mDialog.setOnClickListener(this);
+        mFragment = findViewById(R.id.dialog_fragment);
+        mFragment.setOnClickListener(this);
+        dialogFromBottom = new DialogFromBottom(this);
+        dialogFromBottom.setContentView(dialogContent);
+
     }
 
     private void initPopupWindow() {
@@ -43,8 +59,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         popupWindow.setAnimationStyle(R.style.MyPopWindowAnim);
         //设置PopupWindow的背景
         //popupWindow.setBackgroundDrawable(new ColorDrawable(Color.GRAY));
-       /* TextView dismiss = contentView.findViewById(R.id.dismiss);
-        dismiss.setOnClickListener(this);*/
+        TextView dismiss = contentView.findViewById(R.id.dismiss);
+        dismiss.setOnClickListener(this);
         /*TextView camera = contentView.findViewById(R.id.open_from_camera);
         camera.setOnClickListener(this);
         TextView album = contentView.findViewById(R.id.open_album);
@@ -56,14 +72,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void showPopWindow() {
         //设置是否遮住状态栏
         fitPopupWindowOverStatusBar(true);
-        View rootview = LayoutInflater.from(MainActivity.this).inflate(R.layout.activity_main, null);
-        popupWindow.showAtLocation(rootview, Gravity.BOTTOM, 0, 0);
+        View rootView = LayoutInflater.from(MainActivity.this).inflate(R.layout.activity_main, null);
+        popupWindow.showAtLocation(rootView, Gravity.BOTTOM, 0, 0);
     }
+
     //弹出的窗口是否覆盖状态栏
     public void fitPopupWindowOverStatusBar(boolean needFullScreen) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             try {
-                //利用反射重新mLayoutInScreen的值
+                //利用反射重新设置mLayoutInScreen的值
                 Field mLayoutInScreen = PopupWindow.class.getDeclaredField("mLayoutInScreen");
                 mLayoutInScreen.setAccessible(true);
                 mLayoutInScreen.set(popupWindow, needFullScreen);
@@ -74,12 +91,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.popup_window:
                 showPopWindow();
                 break;
+            case R.id.dialog:
+                DialogFromBottom();
+                break;
+            case R.id.dialog_fragment:
+                DialogFragmentFromBottom();
+                break;
+            case R.id.dismiss:
+                dismissPopWindow();
+                break;
         }
+    }
+
+    private void dismissPopWindow() {
+        if (popupWindow.isShowing() && popupWindow != null) {
+            popupWindow.dismiss();
+        }
+    }
+
+    private void DialogFragmentFromBottom() {
+        showDialog();
+    }
+    void showDialog() {
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        // Create and show the dialog.
+        DialogFragmentFromBottom newFragment = new DialogFragmentFromBottom();
+        newFragment.show(ft, "dialog");
+    }
+    private void DialogFromBottom() {
+       dialogFromBottom.show();
     }
 }
